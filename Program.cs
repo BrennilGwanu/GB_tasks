459e393792fa4afb2732bbd1a086268ac6cb3718
Snake <per.mortis.non.lates@gmail.com>
2023-01-24 22:51:45 +0500

    comlpleted

--- Program.cs
+++ Program.cs
@@ -2,7 +2,6 @@
 
 Console.WriteLine("Введите первое число: ");
 int num1 = int.Parse(Console.ReadLine()!);
-
 Console.WriteLine("Введите второе число: ");
 int num2 = int.Parse(Console.ReadLine()!);
 
